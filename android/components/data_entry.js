0f7833bf070368bc8b57934b30fe0b6291dcc482
DeliSauce <nycdelisauce@gmail.com>
2017-05-12 19:36:30 -0700

    Update details overview grid: add row/column headers and represents true dataset size.

--- android/components/data_entry.js
+++ android/components/data_entry.js
@@ -4,16 +4,52 @@ import {
   View,
   Text,
   StyleSheet,
+  ScrollView,
+  FlatList,
   Button
 } from 'react-native';
 
-const RowComponent = ({row, numCols}) => {
+const HeaderComponent = ({categories}) => {
   const thisRow = [];
-  for(let i = 0; i < numCols; i++) {
-    thisRow.push(
-      <View key={row+"_"+i} style={{width: 30, height: 30, borderWidth: 1, borderColor: 'blue'}}></View>
-    );
+
+  for(let i = 0; i <= categories.length; i++) {
+    if (i === 0) {
+      thisRow.push(
+        <View key={i} style={{width: 90, height: 90}}/>
+      );
+    } else {
+      thisRow.push(
+        <View key={i} style={{width: 90, height: 90, justifyContent: 'center', alignItems: 'center'}}>
+          <Text> {"Cat: " + i} </Text>
+        </View>
+      );
+    }
   }
+  return (
+    <View style={{flexDirection: 'row'}}>
+      {thisRow}
+    </View>
+  );
+};
+
+const RowComponent = ({row, numCols, boxSize}) => {
+  const thisRow = [];
+  let day = row + 1;
+
+  for(let i = 0; i <= numCols; i++) {
+    if (i === 0) {
+      thisRow.push(
+        <View key={row+"_"+i} style={{width: boxSize, height: boxSize, justifyContent: 'center', alignItems: 'center'}}>
+          <Text> {"Day: " + day} </Text>
+        </View>
+      );
+    } else {
+      thisRow.push(
+        <View key={row+"_"+i} style={{width: boxSize, height: boxSize, borderWidth: 1, borderColor: 'blue'}}/>
+      );
+    }
+  }
+
   return (
     <View style={{flexDirection: 'row'}}>
       {thisRow}
@@ -29,14 +65,19 @@ export default class DataEntry extends Component {
       days: 12,
       cats: 7
     };
-    this.state = {board: []};
+    this.days = this.data.dates.length;
+    this.cats = this.data.categories.length;
+    this.state = {
+
+    };
   }
 
   renderBoardViaComponent(numRows, numCols){
+    console.log('dataset: ', this.data);
     const board = [];
-    for(let i = 0; i < numRows; i++) {
+    for(let i = 0; i <= numRows; i++) {
       board.push(
-        <RowComponent row={i} numCols={numCols} rowData={1}/>
+        <RowComponent row={i} numCols={numCols} rowData={1} boxSize={90}/>
       );
     }
     return (board);
@@ -45,18 +86,10 @@ export default class DataEntry extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View>
-
-        </View>
-        {this.renderBoardViaComponent(this.TEST_DATA.days, this.TEST_DATA.cats)}
-        <Text>
-          Data Entry
-         </Text>
-         <Button
-           title={'log challenge data to console'}
-           onPress={()=> {console.log("challenge data", this.data);}}>
-
-         </Button>
+        <HeaderComponent categories={this.data.categories}/>
+        <ScrollView>
+          {this.renderBoardViaComponent(this.days, this.cats)}
+        </ScrollView>
       </View>
     );
   }
