4e0ee0a74c31c26e5047f1fe70af8bc0e959e5fc
DeliSauce <nycdelisauce@gmail.com>
2017-05-13 01:32:21 -0700

    Add/update challenge data entry screen. Next steps: get data entry to render (fix renderCategories and handleCatSwitch methods) and settle on db structure for data entry object.

--- android/components/challenge_data_entry.js
+++ android/components/challenge_data_entry.js
@@ -3,15 +3,58 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  FlatList,
+  Switch
 } from 'react-native';
 
 
 export default class ChallengeDataEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.data = this.props.navigation.state.params.challengeData;
+    this.categories = this.data.categories;
+    // this.days = this.data.days;
+    // this.cats = this.data.categories.length;
+    this.state = {
+
+    };
+  }
+
+  //TODO need to actually get this method working
+  handleCatSwitch(catObj, idx, bool) {
+    this.setState((previousState) => {
+      previousState.categoryOptions[idx].status = bool;
+      return previousState;
+    });
+  }
+
+  //TODO need to actually get this method working
+  renderCategories({item, index}) {
+    console.log('HIT RENDER CATEGORIES');
+    return(
+      <View
+        key={index}
+        >
+        <Text style={{fontSize: 20}}> {item} </Text>
+        <Switch
+          value={false}
+          onValueChange={(bool) => this.handleCatSwitch(item, index, bool)}
+          />
+      </View>
+    );
+  }
 
   render() {
     return (
       <View style={styles.container}>
+        <FlatList
+        data={this.categories}
+        extraData={this.state}
+        renderItem={catObj => this.renderCategories(catObj)}
+        >
+        </FlatList>
+
         <Text>
           Challenge Data Entry
          </Text>
