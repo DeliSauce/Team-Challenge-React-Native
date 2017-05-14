bb1416f1e70fb95b8db920fc49893e61b7cb03c5
DeliSauce <nycdelisauce@gmail.com>
2017-05-13 21:44:28 -0700

    Update challenge overview to render real data. There are still issues with db sync across screens and db

--- android/components/challenge_overview.js
+++ android/components/challenge_overview.js
@@ -9,17 +9,17 @@ import {
   Button
 } from 'react-native';
 
-const HeaderComponent = ({categories}) => {
+const HeaderComponent = ({categories, boxSize}) => {
   const thisRow = [];
 
   for(let i = 0; i <= categories.length; i++) {
     if (i === 0) {
       thisRow.push(
-        <View key={i} style={{width: 90, height: 90}}/>
+        <View key={i} style={{width: boxSize, height: boxSize}}/>
       );
     } else {
       thisRow.push(
-        <View key={i} style={{width: 90, height: 90, justifyContent: 'center', alignItems: 'center'}}>
+        <View key={i} style={{width: boxSize, height: boxSize, justifyContent: 'center', alignItems: 'center'}}>
           <Text> {"Cat: " + i} </Text>
         </View>
       );
@@ -32,11 +32,13 @@ const HeaderComponent = ({categories}) => {
   );
 };
 
-const RowComponent = ({row, numCols, boxSize}) => {
+const RowComponent = ({row, numCols, rowData, boxSize}) => {
+  console.log("print rowdata", row, rowData);
   const thisRow = [];
   let day = row + 1;
-
+  let fillColor = '';
   for(let i = 0; i <= numCols; i++) {
+
     if (i === 0) {
       thisRow.push(
         <View key={row+"_"+i} style={{width: boxSize, height: boxSize, justifyContent: 'center', alignItems: 'center'}}>
@@ -44,8 +46,9 @@ const RowComponent = ({row, numCols, boxSize}) => {
         </View>
       );
     } else {
+      fillColor = (rowData[i - 1] ? 'red' : 'white');
       thisRow.push(
-        <View key={row+"_"+i} style={{width: boxSize, height: boxSize, borderWidth: 1, borderColor: 'blue'}}/>
+        <View key={row+"_"+i} style={{width: boxSize, height: boxSize, borderWidth: 1, backgroundColor: fillColor}}/>
       );
     }
   }
@@ -61,23 +64,20 @@ export default class ChallengeOverview extends Component {
   constructor(props) {
     super(props);
     this.data = this.props.navigation.state.params.challengeData;
-    this.TEST_DATA = {
-      days: 12,
-      cats: 7
-    };
     this.days = this.data.days;
+    this.boxSize = 60;
     this.cats = this.data.categories.length;
-    this.state = {
-
-    };
+    this.userData = this.data.userData[this.data.adminID];
+    // console.log(this.userData);
+    this.state = {};
   }
 
   renderBoardViaComponent(numRows, numCols){
     console.log('dataset: ', this.data);
     const board = [];
-    for(let i = 0; i <= numRows; i++) {
+    for(let i = 0; i < numRows; i++) {
       board.push(
-        <RowComponent row={i} numCols={numCols} rowData={1} boxSize={90}/>
+        <RowComponent row={i} numCols={numCols} rowData={this.userData[i]} boxSize={this.boxSize}/>
       );
     }
     return (board);
@@ -86,7 +86,7 @@ export default class ChallengeOverview extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <HeaderComponent categories={this.data.categories}/>
+        <HeaderComponent categories={this.data.categories} boxSize={this.boxSize}/>
         <ScrollView>
           {this.renderBoardViaComponent(this.days, this.cats)}
         </ScrollView>
