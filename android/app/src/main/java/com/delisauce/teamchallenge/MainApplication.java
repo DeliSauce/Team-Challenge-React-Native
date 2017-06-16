475c5e9b87b988e51bc8c3e5288024ac7635255a
DeliSauce <nycdelisauce@gmail.com>
2017-06-16 16:21:03 -0700

    successfully link fbsdk module and edit android files for use of facebook login (still need to edit ios files)

--- android/app/src/main/java/com/delisauce/teamchallenge/MainApplication.java
+++ android/app/src/main/java/com/delisauce/teamchallenge/MainApplication.java
@@ -3,6 +3,7 @@ package com.delisauce.teamchallenge;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -15,6 +16,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -25,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new FBSDKPackage(mCallbackManager),
           new VectorIconsPackage(),
           new ReactNativeContacts()
       );
@@ -39,6 +47,9 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
